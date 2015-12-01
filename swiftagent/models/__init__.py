bad3bdbd55945e096f6da89245640d9dbc33f6e8
Tim Burke <tim.burke@gmail.com>
2015-11-30 16:53:48 -0800

    Flesh out some more ideas

--- swiftagent/models/__init__.py
+++ swiftagent/models/__init__.py
@@ -2,3 +2,5 @@
 from swiftagent.models.exceptions import SwiftClientError
 from swiftagent.models.cluster import Cluster
 from swiftagent.models.account import Account
+#from swiftagent.models.container import Container
+#from swiftagent.models.object import Object
